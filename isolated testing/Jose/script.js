e6c9b1ec7affc943a9ee99449fa8e6b6f85d65cc
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2024-02-17 16:29:03 -0800

    experimental new animated menu

---
+++ isolated testing/Jose/script.js
@@ -0,0 +1,7 @@
+document.getElementById("menuBtn").onclick = ()=>{
+    document.getElementById("menus").style.width = "50vmin"
+}
+document.getElementById("close").onclick = ()=>{
+    document.getElementById("menus").style.width = "0vmin"
+    
+}
