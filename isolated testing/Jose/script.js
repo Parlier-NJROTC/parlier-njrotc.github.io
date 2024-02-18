00429d28edb908a5ebce44d11f95809a24d753a2
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2024-02-17 18:25:05 -0800

    animations look nice, the merge is gonna be a pain on monday

--- isolated testing/Jose/script.js
+++ isolated testing/Jose/script.js
@@ -1,7 +1,10 @@
 document.getElementById("menuBtn").onclick = ()=>{
     document.getElementById("menus").style.width = "50vmin"
+    document.getElementById("blur").style.display = "block"
+    document.getElementById("blur").style.opacity = "0.5"
 }
 document.getElementById("close").onclick = ()=>{
-    document.getElementById("menus").style.width = "0vmin"
-    
+    document.getElementById("menus").style.width = "0vmin"   
+    document.getElementById("blur").style.display = "none"
+    document.getElementById("blur").style.opacity = 0
 }
