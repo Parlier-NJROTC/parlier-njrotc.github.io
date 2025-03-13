bee6680a356ac7b5e2c43bbc3a0967cdc8006a2d
SkyPrime <skyn21prime@gmail.com>
2025-03-12 19:35:47 -0700

    Youngest to Oldest

--- generate-image-list.cjs
+++ generate-image-list.cjs
@@ -13,7 +13,8 @@ const path =  require('path');
 let fileConfig = {
    q: `'${folderId}' in parents AND (mimeType='image/jpeg' OR mimeType='image/png' OR mimeType='video/mp4')`,
    pageSize: 30,
-   key: apiKey
+   key: apiKey,
+   orderBy: 'createdTime desc' // This Makes it to Youngest to Oldest
 };
 
 let currentPageToken = null;
