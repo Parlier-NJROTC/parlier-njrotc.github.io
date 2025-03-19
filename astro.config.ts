72b306ffc01f72859354e5acbc3629e09e9d9b2d
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2025-03-18 22:08:43 -0700

    added better navigation, fixed bar for firefox. added ids to individual buttons

--- astro.config.ts
+++ astro.config.ts
@@ -4,5 +4,9 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  redirects: {
+    "/Gallery/Page--1": "/Gallery",
+    "/Gallery/Page-0": "/Gallery"
+  }
 });
--- src/components/ControlBar/ControlBar.svelte
+++ src/components/ControlBar/ControlBar.svelte
@@ -2,23 +2,50 @@
     import data from '../../data/gallery.json'
     import "./ControlBar.less"
 
+    export let CurrentPageNum:number
+
     let buttons = []
 
     for(let i = 0;i<data.length;i++){
         buttons.push(data[i].page)
     }
 
+    let NextPage = `/Gallery/Page-${CurrentPageNum+1}`
+    let NextDisabled = false
+    let PrevPage = `/Gallery/Page-${CurrentPageNum-1}`
+    let PrevDisabled = false
+    if(CurrentPageNum<=0){
+        // hide or removeControlBar-Prev-Link
+        PrevPage = ``
+        PrevDisabled = true
+
+    }
+    if(CurrentPageNum>=buttons.length){
+        // hide or remove ControlBar-Next-Link
+        NextPage = ``
+        NextDisabled = true
+    }
 </script>
 
 <div id="Control-BackPlate">
+        <button id="ControlBar-Previous" disabled={PrevDisabled}>
+            <a id="ControlBar-Prev-Link" href={PrevPage} aria-disabled={PrevDisabled}>
+                &lt; <br> Previous Page
+            </a>
+        </button>
     <div id="ControlBar">
         <a href="/Gallery">
             Home
         </a>
         {#each buttons as link}
-            <a href={"/Gallery/Page-"+link}>
+            <a href={"/Gallery/Page-"+link} id={`ControlBar-Button${link}`}>
                 {link}
             </a>
         {/each}
     </div>
+        <button id="ControlBar-Next" disabled={NextDisabled}>
+            <a id="ControlBar-Next-Link" href={NextPage} aria-disabled={NextDisabled}>
+                &gt; <br> Next Page
+            </a>
+        </button>
 </div>
